5dafe47d5e6e2453992824a60fee4a7cb6397765
Myra Malik <myramalik@Myras-MacBook-Air.local>
2023-10-18 18:17:22 +0530

    eternal pain

--- Assignment 3/avltree.c
+++ Assignment 3/avltree.c
@@ -1,160 +1,230 @@
-#include <stdio.h>  
-#include <stdlib.h>  
-   
-struct Node  
-{  
-    int key;  
-    struct Node *left;  
-    struct Node *right;  
-    int height;  
-};  
-   
-int getHeight(struct Node *n){  
-    if(n==NULL)  
-        return 0;  
-    return n->height;  
-}  
-   
-struct Node *createNode(int key){  
-    struct Node* node = (struct Node *) malloc(sizeof(struct Node));  
-    node->key = key;  
-    node->left = NULL;  
-    node->right = NULL;  
-    node->height = 1;  
-    return node;  
-}  
+#include <stdio.h>
+#include <stdlib.h>
 
-void displayTree(struct Node* root, int space)
-{
-    int i;
+struct Node {
+    int key;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
 
-    if (root == NULL) return;
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int getHeight(struct Node *n) {
+    if (n == NULL)
+        return 0;
+    return n->height;
+}
+
+struct Node *createNode(int key) {
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return node;
+}
+
+struct Node *rightRotate(struct Node *y) {
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    return x;
+}
+
+struct Node *leftRotate(struct Node *x) {
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    return y;
+}
+
+int getBalanceFactor(struct Node *n) {
+    if (n == NULL) {
+        return 0;
+    }
+    return getHeight(n->left) - getHeight(n->right);
+}
+
+struct Node *insert(struct Node *node, int key) {
+    if (node == NULL)
+        return createNode(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+    int bf = getBalanceFactor(node);
+
+    if (bf > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if (bf > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (bf < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    if (bf < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+struct Node *minValueNode(struct Node *node) {
+    struct Node *current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+struct Node *deleteNode(struct Node *root, int key) {
+    if (root == NULL)
+        return root;
+
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else {
+        if ((root->left == NULL) || (root->right == NULL)) {
+            struct Node *temp = root->left ? root->left : root->right;
+
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            } else
+                *root = *temp;
+
+            free(temp);
+        } else {
+            struct Node *temp = minValueNode(root->right);
+
+            root->key = temp->key;
+
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+    int bf = getBalanceFactor(root);
+
+    if (bf > 1 && getBalanceFactor(root->left) >= 0)
+        return rightRotate(root);
+
+    if (bf > 1 && getBalanceFactor(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    if (bf < -1 && getBalanceFactor(root->right) <= 0)
+        return leftRotate(root);
+
+    if (bf < -1 && getBalanceFactor(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+void inOrder(struct Node *root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        printf("%d ", root->key);
+        inOrder(root->right);
+    }
+}
+
+void displayTree(struct Node *root, int space) {
+    if (root == NULL)
+        return;
 
     space += 10;
 
-    // Process right child first
     displayTree(root->right, space);
 
-    // Print current node after space count
     printf("\n");
-    for (i = 10; i < space; i++)
+    for (int i = 10; i < space; i++)
         printf(" ");
     printf("%d\n", root->key);
 
-    // Process left child
     displayTree(root->left, space);
 }
-   
-int max (int a, int b){  
-    return (a>b)?a:b;  
-}  
-   
-int getBalanceFactor(struct Node * n){  
-    if(n==NULL){  
-        return 0;  
-    }  
-    return getHeight(n->left) - getHeight(n->right);  
-}  
-   
-struct Node* rightRotate(struct Node* y){  
-    struct Node* x = y->left;  
-    struct Node* T2 = x->right;  
-   
-    x->right = y;  
-    y->left = T2;  
-   
-    x->height = max(getHeight(x->right), getHeight(x->left)) + 1;  
-    y->height = max(getHeight(y->right), getHeight(y->left)) + 1;  
-   
-    return x;  
-}  
-   
-struct Node* leftRotate(struct Node* x){  
-    struct Node* y = x->right;  
-    struct Node* T2 = y->left;  
-   
-    y->left = x;  
-    x->right = T2;  
-   
-    x->height = max(getHeight(x->right), getHeight(x->left)) + 1;  
-    y->height = max(getHeight(y->right), getHeight(y->left)) + 1;  
-   
-    return y;  
-}  
-   
-struct Node *insert(struct Node* node, int key){  
-    if (node == NULL)  
-        return  createNode(key);  
-   
-    if (key < node->key)  
-        node->left  = insert(node->left, key);  
-    else if (key > node->key)  
-        node->right = insert(node->right, key);  
-   
-    node->height = 1 + max(getHeight(node->left), getHeight(node->right));  
-    int bf = getBalanceFactor(node);  
-   
-    // Left Left Case  
-        if(bf>1 && key < node->left->key){  
-            return rightRotate(node);  
-        }  
-    // Right Right Case  
-        if(bf<-1 && key > node->right->key){  
-            return leftRotate(node);  
-        }  
-    // Left Right Case  
-    if(bf>1 && key > node->left->key){  
-            node->left = leftRotate(node->left);  
-            return rightRotate(node);  
-        }  
-    // Right Left Case  
-    if(bf<-1 && key < node->right->key){  
-            node->right = rightRotate(node->right);  
-            return leftRotate(node);  
-        }  
-    return node;  
-}  
-   
-void preOrder(struct Node *root)  
-{  
-    if(root != NULL)  
-    {  
-        printf("%d ", root->key);  
-        preOrder(root->left);  
-        preOrder(root->right);  
-    }  
-}  
 
-void inOrder(struct Node *root)  
-{  
-    if(root != NULL)  
-    {  
-        preOrder(root->left);  
-        printf("%d ", root->key);  
-        preOrder(root->right);  
-    }  
-}  
-   
-int main(){  
-    struct Node * root = NULL;  
-   
-   
-    root = insert(root, 10);  
-    root = insert(root, 20);  
-    root = insert(root, 40);  
-    root = insert(root, 50);  
-    root = insert(root, 60);  
-    root = insert(root, 30);  
-    root = insert(root, 70);  
-    root = insert(root, 80);  
-    root = insert(root, 90);  
-    preOrder(root);  
-    printf("\n");
+int main() {
+    struct Node *root = NULL;
+    int elm, menu;
 
-    inOrder(root);  
-    printf("\n");
+    while (1) {
+        printf("0. Exit, 1. Initialize, 2. Search, 3. Insert, 4. Delete, 5. Inorder Traversal. Input Choice: ");
+        scanf("%d", &menu);
 
-    displayTree(root, 10);
+        switch (menu) {
+            case 0:
+                exit(0);
 
-    return 0;  
-}  
+            case 1:
+                root = NULL;
+                break;
+
+            case 2:
+                printf("Give element to search: ");
+                scanf("%d", &elm);
+                struct Node *searchResult = search(root, elm);
+                if (searchResult == NULL)
+                    printf("Element not found\n");
+                else
+                    printf("Element found: %d\n", searchResult->key);
+                break;
+
+            case 3:
+                printf("Give element to insert: ");
+                scanf("%d", &elm);
+                root = insert(root, elm);
+                printf("Element inserted: %d\n", elm);
+                break;
+
+            case 4:
+                printf("Give element to delete: ");
+                scanf("%d", &elm);
+                root = deleteNode(root, elm);
+                printf("Element deleted: %d\n", elm);
+                break;
+
+            case 5:
+                printf("Inorder Traversal: ");
+                inOrder(root);
+                printf("\n");
+                break;
+
+            default:
+                printf("Wrong choice\n");
+        }
+    }
+
+    return 0;
+}
